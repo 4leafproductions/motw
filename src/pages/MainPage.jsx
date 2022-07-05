4e203708bfbfebc127d5627b741fd3782279d71c
Andrew <viloca42@gmail.com>
2022-07-04 20:59:07 -0500

    pen and paper table

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,9 +2,15 @@ import React, { useState } from 'react';
 import spray from '../img/sprayBG.png';
 import Modal from '../components/Modal';
 import HeaderNav from '../components/HeaderNav';
+import ResultsTable from '../components/ResultsTable';
 import './Main.css';
 const MainPage = (props) => {
   const [openModal, setOpenModal] = useState(false);
+  const header = ['Series', 'Keeper'];
+  const seriesResults = [
+    {id: 1, name: 'Hello World Series', keeper: 'coolKeeper'},
+    {id: 2, name: 'Another Series', keeper: 'veryCoolKeeper'}
+  ];
   return (
     <>
       <HeaderNav></HeaderNav>
@@ -12,6 +18,7 @@ const MainPage = (props) => {
         <div className="mainHeader sprayed">
           <h2>Series Select</h2>
         </div>
+        <ResultsTable header={header} results={seriesResults}></ResultsTable>
       </div>
     </>
   )
